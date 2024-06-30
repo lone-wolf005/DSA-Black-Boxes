500ae2c59fa3b7addcbb3d9eac1e7b29afac8f2b
JIGAR THAKOR <116433484+lone-wolf005@users.noreply.github.com>
2024-06-30 23:34:50 +0530

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,25 @@
+const int N = 1000; // Adjust the size as needed
+
+int parent[N];
+int size[N];
+
+void make(int v) {
+    parent[v] = v;
+    size[v] = 1;
+}
+
+int find(int v) {
+    if (v == parent[v]) return v;
+    return parent[v] = find(parent[v]);
+}
+
+void Union(int a, int b) {
+    a = find(a);
+    b = find(b);
+    if (a != b) {
+        if (size[a] < size[b])
+            swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+    }
+}
