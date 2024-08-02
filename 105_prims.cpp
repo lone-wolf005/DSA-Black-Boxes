769397f5707fce90e01d66e3955932c873d98f6d
JIGAR THAKOR <116433484+lone-wolf005@users.noreply.github.com>
2024-08-02 11:35:45 +0530

    Update 105_prims.cpp

--- 105_prims.cpp
+++ 105_prims.cpp
@@ -1,90 +1,65 @@
-#include <bits/stdc++.h> 
-vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g)
-{
-    unordered_map<int,vector<pair<int,int>>>adj;
-    for(auto x:g){
-        int u = x.first.first;
-        int v = x.first.second;
-        int w = x.second;
-        adj[u].push_back({v,w});
-        adj[v].push_back({u,w});
+//Radhe Radhe
+#include<bits/stdc++.h>
+using namespace std;
+const int M = 1e9+7;
+#define ll long long int
+int Factorial[500000+10];
+class Node{
+    // private:
+    public:
+    int wt,node,parent;
+    Node(int wt,int node,int parent){
+        this->wt = wt;
+        this->node = node;
+        this->parent = parent;
     }
-    vector<bool>mst(n+1);
-    vector<int>parent(n+1);
-    vector<int>key(n+1);
-    for(int i=0;i<=n;i++)key[i]=INT_MAX;
-    key[1]=0;
-    // parent[0]
+};
 
-    for(int i=1;i<=n;i++){
-        int u;
-        int mini = INT_MAX;
-        for(int j=1;j<=n;j++){
-            if(!mst[j]&&key[j]<mini){
-                mini=key[j];
-                u=j;
-            }
-        }
-        mst[u]=1;
-        for(auto x:adj[u]){
-            int v = x.first;
-            int w = x.second;
-            if(!mst[v] && w < key[v]){
-                key[v]=w;
-                parent[v]=u;
-            }
-        } 
+int prims(int src,int n,vector<pair<int,int>>adj[],vector<vector<int>>&mst){
+    
+    priority_queue<Node*,vector<Node*>,greater<Node*>>pq;
+    pq.push(new Node(0,0,-1));
+    vector<int>visited(n,0);
+    int sum = 0;
+
+    while(!pq.empty()){
+         auto tp = pq.top();
+         pq.pop();
+         int wt = tp->wt;
+         int node = tp->node;
+         int parent = tp->parent;
+         if(!visited[node]){
+             visited[node] = 1;
+             sum+=wt;
+             if(parent !=-1){
+                mst.push_back({node,parent});
+             }
+         }
+         for(auto x:adj[node]){
+             if(!visited[x.first]){
+                  pq.push(new Node(x.second,x.first,node));
+             }
+         }
+
     }
-    vector<pair<pair<int, int>, int>>ans;
-    for(int i=2;i<=n;i++){
-        ans.push_back({{i,parent[i]},key[i]});
-    }
-return ans;
+    return sum;
 }
-
-
-// o(nlogn)
-
-
-// #include <bits/stdc++.h> 
-// vector<pair<pair<int, int>, int>> calculatePrimsMST(int n, int m, vector<pair<pair<int, int>, int>> &g)
-// {
-//     unordered_map<int,vector<pair<int,int>>>adj;
-//     for(auto x:g){
-//         int u = x.first.first;
-//         int v = x.first.second;
-//         int w = x.second;
-//         adj[u].push_back({v,w});
-//         adj[v].push_back({u,w});
-//     }
-//     vector<bool>mst(n+1);
-//     vector<int>parent(n+1);
-//     vector<int>key(n+1);
-//     for(int i=0;i<=n;i++)key[i]=INT_MAX;
-//     key[1]=0;
-//     // parent[0]
-//     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
-//     q.push({0,1});
-
-//     while(!q.empty()){
-//         auto fr = q.top();
-//         q.pop();
-//         int u = fr.second;
-//         if(mst[u])continue;
-//         mst[u]=1;
-//         for(auto x:adj[u]){
-//             int v = x.first;
-//             int w = x.second;
-//             if(!mst[v] && w < key[v]){
-//                 key[v]=w;
-//                 parent[v]=u;
-//                 q.push({w,v});
-//             }
-//         } 
-//     }
-//     vector<pair<pair<int, int>, int>>ans;
-//     for(int i=2;i<=n;i++){
-//         ans.push_back({{i,parent[i]},key[i]});
-//     }
-// return ans;
-// }
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<pair<int,int>>adj[n];
+    for(int i=0;i<m;i++){
+        int a,b,wt;
+        cin>>a>>b>>wt;
+        adj[a].push_back({b,wt});
+        adj[b].push_back({a,wt});
+    }
+    vector<vector<int>>mst;
+    int sum = prims(0,n,adj,mst);
+    cout<<sum<<endl;
+    for(auto x:mst){
+        cout<<x[0]<<" "<<x[1]<<endl;
+    }
+    
+return 0;
+}
